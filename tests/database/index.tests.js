ec1f2de349454150fb40264a74d487c78d9df706
Zababurin Sergei <zababurins@vk.com>
2022-08-20 03:48:01 +0300

    update

--- tests/database/index.tests.js
+++ tests/database/index.tests.js
@@ -3,7 +3,7 @@ import feedTestLoad from '/tests/database/feed-load.test.js';
 import manual from '/tests/database/orbit-db-test-utils/test/manual.spec.js';
 import memstore from '/tests/database/orbit-db-test-utils/test/memstore.spec.js';
 
-memstore()
-manual()
-feedTestLoad()
-feedTest()
+memstore();
+manual();
+// feedTestLoad();
+// feedTest();
